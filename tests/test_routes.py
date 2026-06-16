5005959e42cc7dff1e2f27723e28a70e05f01fb7
senthamil-pandiyan <senthamil.pandiyan@gov.scot>
2026-06-16 21:13:05 +0100

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,8 +7,7 @@ Test cases can be run with the following:
 """
 from unittest import TestCase
 from service.common import status  # HTTP Status Codes
-from service.routes import reset_counters
-import app
+from service.routes import app, reset_counters
 
 
 ######################################################################
