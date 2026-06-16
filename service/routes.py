e6cbc9b3461a8d2b9ac03337ed9325d5e360c8d4
senthamil-pandiyan <senthamil.pandiyan@gov.scot>
2026-06-16 18:49:12 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -3,7 +3,7 @@ Controller for routes
 """
 from flask import jsonify, url_for, abort
 from service import app
-# from service.common import status
+from service.common import status
 
 COUNTER = {}
 
