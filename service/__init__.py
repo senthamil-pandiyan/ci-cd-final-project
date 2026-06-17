395da0007847e012c81221e81687808622d58e7b
senthamil-pandiyan <senthamil.pandiyan@gov.scot>
2026-06-17 11:14:26 +0100

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -13,7 +13,7 @@ from service.common import log_handlers  # noqa: E402
 log_handlers.init_logging(app, "gunicorn.error")
 
 app.logger.info(70 * "-")
-app.logger.info("  S E R V I C E  IS   R U N N I N G  ".center(70, "*"))
+app.logger.info("  S E R V I C E  R U N N I N G  ".center(70, "*"))
 app.logger.info(70 * "-")
 
 __all__ = ["app"]
